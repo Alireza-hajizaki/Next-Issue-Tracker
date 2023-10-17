4d03cc21075ec39dc2ac5f5c475b208b6915a00e
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-10-17 21:07:31 +0330

    crated issuePage & use simplemde editor

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Flex, Text, Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const IssuesPage = () => {
   return (
-    <div>IssuesPage</div>
-  )
-}
+    <Flex direction="column" gap="4">
+      <Text>Hello from Radix Themes :)</Text>
+      <div><Button><Link href='/issues/new'>New Issue</Link></Button></div>
+    </Flex>
+  );
+};
 
-export default IssuesPage
+export default IssuesPage;
